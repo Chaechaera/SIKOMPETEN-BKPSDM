e7d64f1e3e8475b88336debc777fda946602b467
ficalidya08 <ficalidya09@gmail.com>
2026-06-17 22:03:26 +0700

    fix SuperadminController.php

--- app/Izin/Http/Controllers/Auth/SuperadminController.php
+++ app/Izin/Http/Controllers/Auth/SuperadminController.php
@@ -9,7 +9,6 @@ use App\Izin\Models\Izin_Sertifikats;
 use App\Izin\Models\Izin_Usulankegiatans;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\Izin\Models\Izin_Laporankegiatans;
 
 class SuperadminController extends Controller
 {
@@ -159,6 +158,29 @@ class SuperadminController extends Controller
             : 0;
 
 
+        // TOTAL
+        $total = Izin_Laporankegiatans::count();
+
+        // DISETUJUI
+        $disetujui = Izin_Laporankegiatans::get()
+            ->filter(fn($item) =>
+                in_array($item->status_laporan_ui, ['accepted', 'finish'])
+            )->count();
+
+        // MENUNGGU (FIX sesuai kebutuhan kamu)
+        $menunggu = Izin_Laporankegiatans::get()
+            ->filter(fn($item) =>
+                $item->status_laporan_ui === 'need_review'
+            )->count();
+
+        // DATA TERBARU
+        $laporans = Izin_Laporankegiatans::with([
+            'inputlaporankegiatans.inputusulankegiatans.usulankegiatans.subunitkerjas'
+        ])
+        ->latest()
+        ->take(5)
+        ->get();
+
         return view('pages.dashboard.superadmin', compact(
             'totalUsulan',
             'usulanPending',
@@ -188,37 +210,12 @@ class SuperadminController extends Controller
             'persenLaporanDitolak',
             'persenLaporanPesertaDisetujui',
             'persenLaporanPesertaDitolak',
+
+            'total',
+            'disetujui',
+            'menunggu',
+            'laporans'
         ));
     }
-{
-    // TOTAL
-    $total = Izin_Laporankegiatans::count();
-
-    // DISETUJUI
-    $disetujui = Izin_Laporankegiatans::get()
-        ->filter(fn($item) =>
-            in_array($item->status_laporan_ui, ['accepted', 'finish'])
-        )->count();
-
-    // MENUNGGU (FIX sesuai kebutuhan kamu)
-    $menunggu = Izin_Laporankegiatans::get()
-        ->filter(fn($item) =>
-            $item->status_laporan_ui === 'need_review'
-        )->count();
-
-    //DATA TERBARU
-    $laporans = Izin_Laporankegiatans::with([
-        'inputlaporankegiatans.inputusulankegiatans.usulankegiatans.subunitkerjas'
-    ])
-    ->latest()
-    ->take(5)
-    ->get();
-
-    return view('pages.dashboard.superadmin', compact(
-        'total',
-        'disetujui',
-        'menunggu',
-        'laporans'
-    ));
 }
 
